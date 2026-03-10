7a6602d384807575bda3d76a25289e59d29177f7
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-03-11 00:32:45 +0500

    feat: extend WeatherData model with windDegree, weatherMain, and min/max temps

--- lib/ui/home/model/weather_data.dart
+++ lib/ui/home/model/weather_data.dart
@@ -3,13 +3,17 @@ class WeatherData {
   String temperature;
   double tempRaw;
   double feelsLikeRaw;
+  double tempMinRaw;
+  double tempMaxRaw;
   String cityAndCountry;
   String weatherConditionIconUrl;
   String weatherConditionIconDescription;
+  String weatherMain;
   String humidity;
   String pressure;
   String visibility;
   String wind;
+  double windDegree;
   String feelsLike;
   String sunrise;
   String sunset;
@@ -19,13 +23,17 @@ class WeatherData {
     required this.temperature,
     required this.tempRaw,
     required this.feelsLikeRaw,
+    required this.tempMinRaw,
+    required this.tempMaxRaw,
     required this.cityAndCountry,
     required this.weatherConditionIconUrl,
     required this.weatherConditionIconDescription,
+    required this.weatherMain,
     required this.humidity,
     required this.pressure,
     required this.visibility,
     required this.wind,
+    required this.windDegree,
     required this.feelsLike,
     required this.sunrise,
     required this.sunset,
