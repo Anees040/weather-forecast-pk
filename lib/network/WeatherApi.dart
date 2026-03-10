dbbd4d68190814f0f4117bdf8469e41302ea6041
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-03-11 00:34:56 +0500

    feat: add getForecast method to WeatherApi abstract class

--- lib/network/WeatherApi.dart
+++ lib/network/WeatherApi.dart
@@ -1,5 +1,7 @@
+import 'package:weather_forecast_pk/ui/home/model/forecast_day.dart';
 import 'package:weather_forecast_pk/ui/home/model/weather_data.dart';
 
 abstract class WeatherApi {
   Future<WeatherData>? getWeatherInfo(int? cityId);
+  Future<List<ForecastDay>> getForecast(int? cityId);
 }
