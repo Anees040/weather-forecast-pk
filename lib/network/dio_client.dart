6597cf9c1500ffa77223824bb6a61f19e5552f33
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-03-12 23:34:58 +0500

    Increase network timeout settings

--- lib/network/dio_client.dart
+++ lib/network/dio_client.dart
@@ -15,8 +15,8 @@ class DioClient {
   DioClient._internal() {
     var options = BaseOptions(
       baseUrl: BuildConfig.instance.config.baseUrl,
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 3),
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 10),
     );
 
     _dio = Dio(options);
