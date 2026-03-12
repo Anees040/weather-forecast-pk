86fcf7efa805b2b82b8dab9a4888e06101342ea8
Anees <sp23-bse-030@isbstudent.comsats.edu.pk>
2026-03-12 23:34:58 +0500

    Extend weather API for hourly forecast

--- lib/network/WeatherApi.dart
+++ lib/network/WeatherApi.dart
@@ -1,7 +1,9 @@
 import 'package:weather_forecast_pk/ui/home/model/forecast_day.dart';
+import 'package:weather_forecast_pk/ui/home/model/hourly_forecast.dart';
 import 'package:weather_forecast_pk/ui/home/model/weather_data.dart';
 
 abstract class WeatherApi {
   Future<WeatherData>? getWeatherInfo(int? cityId);
   Future<List<ForecastDay>> getForecast(int? cityId);
+  Future<List<HourlyForecast>> getHourlyForecast(int? cityId);
 }
